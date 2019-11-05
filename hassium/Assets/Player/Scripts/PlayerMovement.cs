7503dd57316ee1e2bfc1b9000546fb14cd05e4a7
Misha <homakhaker@gmail.com>
2019-11-05 22:23:39 +0100

    Revert "Added camera functionality"
    This reverts commit 334c8679aeb53e0994f1caa464517926f7044f27.

--- hassium/Assets/Player/Scripts/PlayerMovement.cs
+++ hassium/Assets/Player/Scripts/PlayerMovement.cs
@@ -6,7 +6,7 @@ public class PlayerMovement : MonoBehaviour {
     [Range(-1, 1)] [SerializeField] private float verInput;
 
     [Header("References")] [SerializeField]
-    private new Rigidbody rigidbody;
+    private Rigidbody rigidbody;
 
     [SerializeField] private IMovement movement;
     [SerializeField] private AnimatorManager animatorManager;
